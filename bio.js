f3b777f08c1fea93b169a22f268a654846dec228
Evan <eakers@wps-sys.com>
2019-01-29 21:36:07 -0800

    test

---
+++ bio.js
@@ -0,0 +1,11 @@
+'use strict'
+
+//first question 
+var name = prompt('Hello. What is your name?');
+console.log(name);
+
+//second Question
+var time = prompt(name + ' what time is it? ');
+console.log(time);
+
+if (time === );
